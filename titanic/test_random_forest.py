e74423c1158c0f937b52973673efdab495ad29a8
Tunehiko511 <simanezumi1989@gmail.com>
2016-05-18 18:35:45 +0900

    修正 warningの除去

--- titanic/test_random_forest.py
+++ titanic/test_random_forest.py
@@ -2,91 +2,85 @@
 import csv as csv
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+import warnings;
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore"); 
+    import matplotlib.pyplot as plt
+
 
 # 下ごしらえ
 def df_cleaner(df):
-    """
-    Clean up a few variables in the training/test sets.
-    """
-    # Clean up ages. 
+    # 足りない部分は補おう
+    # 年齢
     median_age = np.median(df[(df['Age'].notnull())]['Age'])
-
     for passenger in df[(df['Age'].isnull())].index: #.index = 配列内のnullの場所
     	df.loc[passenger, 'Age'] = median_age
-
-    # Clean up fares.
+    # fare
     median_fare = np.median(df[(df['Fare'].notnull())]['Fare'])
     for passenger in df[(df['Fare'].isnull())].index:
         df.loc[passenger, 'Fare'] = median_fare
 
     # 文字列データを数値データへ
-    # Manually convert values to numeric columns for clarity.
-    # Change the sex to a binary column.
-    df['Sex'][(df['Sex'] == 'male')] = 0
-    df['Sex'][(df['Sex'] == 'female')] = 1
-    df['Sex'][(df['Sex'].isnull())] = 2
-
-    # Transform to categorical data.
-    df['Embarked'][(df['Embarked'] == 'S')] = 0
-    df['Embarked'][(df['Embarked'] == 'C')] = 1
-    df['Embarked'][(df['Embarked'] == 'Q')] = 2
-    df['Embarked'][(df['Embarked'].isnull())] = 3
+    df.loc[(df['Sex'] == 'male'),'Sex'] = 0
+    df.loc[(df['Sex'] == 'female'),'Sex'] = 1
+    df.loc[(df['Sex'].isnull()),'Sex'] = 2
+    df.loc[(df['Embarked'] == 'S'),'Embarked'] = 0
+    df.loc[(df['Embarked'] == 'C'),'Embarked'] = 1
+    df.loc[(df['Embarked'] == 'Q'),'Embarked'] = 2
+    df.loc[(df['Embarked'].isnull()),'Embarked'] = 3
 
     return df
 
+# 提出用csvを作りましょ
+def make_csv(file_path, passengerId, predicts):
+    f = open(file_path, "wb")
+    writer = csv.writer(f)
+    writer.writerow(["PassengerId", "Survived"])
+    for row, survived in zip(passengerId, predicts):
+        writer.writerow([row, survived])
+
+# 作ったモデルの性能をしれべましょ
+def getScore(answer, predicts):
+    sum_p = 0.0
+    total = 0.0
+    for (row, predict) in zip(answer,predicts):
+        if row == predict:
+            sum_p += 1.0
+        total += 1.0
+    return sum_p/total
 
 def main():
-    """
-    Visualization of random forest accuracy as function of
-    the number of tress available in the ensemble.
-    """
-
     # Read in the training data.
     train = pd.read_csv('./data/train.csv')
     test = pd.read_csv("./data/test.csv")
-
-    # Set sampling.(75%をサンプルデータに)
-    sampling = 0.75
-
-    # Clean it up.
-    # Remove unused columns, clean age, and convert gender to binary column.
-    # いらないデータは消す（名前，Id，チケット代，Cabin?） 一次元で適切に
+    # いらないデータ(予想)は消しましょう
     train.drop(['Name', 'PassengerId', 'Ticket', 'Cabin'], axis=1, inplace=True)
     test.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
     # 下ごしらえをする
     train = df_cleaner(train)
     test = df_cleaner(test)
-    # Split it into coordinates.
-    # 75%までの配列と残り25%の配列のtrainとtestを用意
-    x_train = train[:int(len(train) * sampling)][['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
-    y_train = train[:int(len(train) * sampling)][['Survived']]
-    x1_train = train[int(len(train) * sampling):][['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
-    y1_train = train[int(len(train) * sampling):][['Survived']]
+    x_train = train[:][['Pclass', 'Sex','Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+    y_train = train[:][['Survived']]
+    # ランダムフォレストでモデルを作ろう
+    scores =[]
+    for trees in range(1,100):
+        model = RandomForestClassifier(n_estimators=trees)
+        model.fit(x_train, np.ravel(y_train))
+        # 一致率を見よう
+        pre = model.predict(x_train)
+        scores.append(getScore(y_train['Survived'],pre))
+    plt.plot(scores,'-r')
+    plt.show()
     
-    x_test = test[:int(len(test))][['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
-    label = test[:int(len(test))][['PassengerId']]
-    model = RandomForestClassifier(n_estimators=200) 		# 決定木=treesの数作る
-    model.fit(x_train, np.ravel(y_train))
-    pre = model.predict(x1_train)
-    # print y1_train['Survived']
-    sum_p = 0.0
-    total = 0.0
-    for (row, predict) in zip(y1_train['Survived'],pre):
-    	if row == predict:
-	    	sum_p += 1.0
-    	total += 1.0
-    print sum_p/total
+    # 本番のテストデータをお化粧直し
+    x_test = test[:][['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']]
+    label = test[:][['PassengerId']]
+    # modelを使って予測しよう
     output = model.predict(x_test)
-
-
-    f = open("random_forest.csv", "wb")
-    writer = csv.writer(f)
-    writer.writerow(["PassengerId", "Survived"])
-    for row, survived in zip(label['PassengerId'], output.astype(int)):
-    	writer.writerow([row, survived])
-
+    # 提出用csvを作ろう
+    make_csv("./output/random_forest.csv", label['PassengerId'], output.astype(int))
 
 if __name__ == '__main__':
     main()
